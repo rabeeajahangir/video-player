40c34cc2f146cf083c085c56e7d990a601d5563e
rabeea jahangir <rabeeajahangir@gmail.com>
2021-10-20 21:18:06 -0400

    added progress and timestamp

--- script.js
+++ script.js
@@ -4,43 +4,59 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-// Play and Pause Video
-
+// Play & pause video
 function toggleVideoStatus() {
-    if(video.paused) {
-        video.play();
-} else {
+  if (video.paused) {
+    video.play();
+  } else {
     video.pause();
+  }
 }
 
-// Update Play/Pause Icon
+// update play/pause icon
 function updatePlayIcon() {
-    if (video.paused) {
-        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'; 
-} else {
-    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'; 
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
-// Update Progress and TimeStamp
+// Update progress & timestamp
 function updateProgress() {
-    return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get the minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if(mins < video.duration){
+    mins = '0' + String(mins);
+  }
+
+  // Get Seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if(secs < video.duration){
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-    return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-//Stop Video
+// Stop video
 function stopVideo() {
-    video.currentTime = 0;
-    video.pause();
+  video.currentTime = 0;
+  video.pause();
 }
-//Event Listeners
+
+// Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeUpdate', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
